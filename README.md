efb373d812f052bfd4badcc63c19bc048c47ffa5
knakayama <knakayama.sh@gmail.com>
2015-11-16 12:41:50 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,12 @@
+pwd.sh-quick
+============
+
+Quickly search password with [pwd.sh](https://github.com/drduh/pwd.sh)
+
+## License
+
+MIT
+
+## Author
+
+[knakayama](https://github.com/knakayama)
---
+++ pwd.sh-quick.plugin.zsh
@@ -0,0 +1,28 @@
+#!/usr/bin/env zsh
+
+-pwd-sh-quick() {
+  local i
+  for i in "peco" "pwd.sh" "pbcopy"; do
+    if ! type "$i" &>/dev/null; then
+      echo "$i does not found in your \$PATH" 1>&2
+      return 1
+    fi
+  done
+
+  pwd.sh r all \
+    | grep -vE '^($|Done)$' \
+    | peco \
+    | awk '{print $1}' \
+    | pbcopy
+
+  zle accept-line
+  zle -R -c
+}
+
+# Local Variables:
+# mode: Shell-Script
+# sh-indentation: 2
+# indent-tabs-mode: nil
+# sh-basic-offset: 2
+# End:
+# vim: ft=zsh sw=2 ts=2 et
